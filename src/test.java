9303bb721fd48229324660b08e8b5c92c870da93
BNDAD <ral7057@naver.com>
2025-10-15 11:31:53 +0900

    Feat: 수정

--- src/test.java
+++ src/test.java
@@ -1,6 +1,11 @@
 publice class text {
 	public static void main(String[] args) {
 			system.out.println("test 출력")
+			system.out.println("test 출력")
+			system.out.println("test 출력")
+			system.out.println("test 출력")
+			system.out.println("test 출력")
+
 	}
 }
 
